0d64adc02659c2a8af9d7d8e91f7c0418d24aabf
Michael Griffin <mdgriffin064@gmail.com>
2018-02-06 11:39:14 +0000

    Added basic structure

---
+++ src/jri/agile/game/Game.java
@@ -0,0 +1,14 @@
+package jri.agile.game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Game {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
