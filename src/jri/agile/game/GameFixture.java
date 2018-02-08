c838528b0754d11fa2d0d7754d01cbb98fc386b9
Michael Griffin <mdgriffin064@gmail.com>
2018-02-08 09:08:14 +0000

    Added updated test fixtures

--- src/jri/agile/game/GameFixture.java
+++ src/jri/agile/game/GameFixture.java
@@ -1,5 +1,8 @@
 package jri.agile.game;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class GameFixture {
 	
 	public static GameContext gc;
@@ -10,10 +13,240 @@ public class GameFixture {
 		}
 	}
 	
+	public void setup () {
+		Game game = new Game(5,5);
+	}
+	
+	
 	public int getPlayerPositionX () {
-		GameEntity player = gc.game.getPlayer();
+		GameEntity player = GameContext.game.getPlayer();
 		
 		return  player.getCurrentPosition().getXPos();
 	}
+	
+	public int getPlayerPositionY () {
+		GameEntity player = GameContext.game.getPlayer();
+		
+		return  player.getCurrentPosition().getYPos();
+	}
+	
+	public int getWumpusPositionX () {
+		GameEntity wump = GameContext.game.getRick();
+		
+		return  wump.getCurrentPosition().getXPos();
+	}
+	
+	public int getWumpusPositionY () {
+		GameEntity wump = GameContext.game.getRick();
+		
+		return  wump.getCurrentPosition().getYPos();
+	}
+	
+	public String movePlayerOneRoomEastWest () {
+		GameEntity player = GameContext.game.getPlayer();
+		GameContext.game.movePlayer('E');
+		int posY = player.getCurrentPosition().getYPos();
+		int posX = player.getCurrentPosition().getXPos();
+		String fullPosition = "(" + posX + "," + posY + ")"; 
+		GameContext.game.movePlayer('W');
+		int posFinalY = player.getCurrentPosition().getYPos();
+		int posFinalX = player.getCurrentPosition().getXPos();
+		fullPosition += ", (" + posFinalY + "," + posFinalX + ")";
+		return fullPosition;
+	}
+	
+	public String movePlayerOneRoomNorthSouth () {
+		GameEntity player = GameContext.game.getPlayer();
+		GameContext.game.movePlayer('S');
+		int posY = player.getCurrentPosition().getYPos();
+		int posX = player.getCurrentPosition().getXPos();
+		String fullPosition = "(" + posX + "," + posY + ")"; 
+		GameContext.game.movePlayer('N');
+		int posFinalY = player.getCurrentPosition().getYPos();
+		int posFinalX = player.getCurrentPosition().getXPos();
+		fullPosition += ", (" + posFinalY + "," + posFinalX + ")";
+		return fullPosition;
+	}
+	
+	public String moveWumbusOneRoomWestEast() {
+		GameEntity wumbus = GameContext.game.getRick();
+		GameContext.game.moveRick('W');
+		int posY = wumbus.getCurrentPosition().getYPos();
+		int posX = wumbus.getCurrentPosition().getXPos();
+		String fullPosition = "(" + posX + "," + posY + ")"; 
+		GameContext.game.moveRick('E');
+		int posFinalY = wumbus.getCurrentPosition().getYPos();
+		int posFinalX = wumbus.getCurrentPosition().getXPos();
+		fullPosition += ", (" + posFinalY + "," + posFinalX + ")";
+		return fullPosition;
+		
+	}
+	public String moveWumbusOneRoomSouthNorth() {
+		GameEntity wumbus = GameContext.game.getRick();
+		GameContext.game.moveRick('N');
+		int posY = wumbus.getCurrentPosition().getYPos();
+		int posX = wumbus.getCurrentPosition().getXPos();
+		String fullPosition = "(" + posX + "," + posY + ")"; 
+		GameContext.game.moveRick('S');
+		int posFinalY = wumbus.getCurrentPosition().getYPos();
+		int posFinalX = wumbus.getCurrentPosition().getXPos();
+		fullPosition += ", (" + posFinalY + "," + posFinalX + ")";
+		return fullPosition;
+	}
+	public String tryMovePlayerOffBoardWest () {
+		GameEntity player = GameContext.game.getPlayer();
+		GameContext.game.movePlayer('W');
+		int posY = player.getCurrentPosition().getYPos();
+		int posX = player.getCurrentPosition().getXPos();
+		String fullPosition = "(" + posX + "," + posY + ")"; 
+		return fullPosition;
+	}
+	
+	public String tryMovePlayerOffBoardNorth () {
+		GameEntity player = GameContext.game.getPlayer();
+		GameContext.game.movePlayer('N');
+		int posY = player.getCurrentPosition().getYPos();
+		int posX = player.getCurrentPosition().getXPos();
+		String fullPosition = "(" + posX + "," + posY + ")"; 
+		return fullPosition;
+	}
+	public String tryMoveWumbusOffBoardEast () {
+		GameEntity wumbus = GameContext.game.getRick();
+		GameContext.game.moveRick('E');
+		int posY = wumbus.getCurrentPosition().getYPos();
+		int posX = wumbus.getCurrentPosition().getXPos();
+		String fullPosition = "(" + posX + "," + posY + ")"; 
+		return fullPosition;
+	}
+	
+	public String tryMoveWumbusOffBoardSouth () {
+		GameEntity wumbus = GameContext.game.getRick();
+		GameContext.game.moveRick('S');
+		int posY = wumbus.getCurrentPosition().getYPos();
+		int posX = wumbus.getCurrentPosition().getXPos();
+		String fullPosition = "(" + posX + "," + posY + ")"; 
+		return fullPosition;
+	}
+	
+	public boolean TestIfMapHasPitRoom()
+	{
+		Room roomPit =  new Room(1, 0, Room.RoomType.PitRoom);
+		GameContext.game.setRoom(0, 1, roomPit);
+		Room r1 = GameContext.game.getRoom(0, 1);
+		boolean isItPit = r1.hasPit();
+		return isItPit;
+	}
+	
+	public boolean TestIfMapHasBatRoom()
+	{
+		Room roomBat =  new Room(0, 1, Room.RoomType.BatRoom);
+		GameContext.game.setRoom(0, 1, roomBat);
+		Room r1 = GameContext.game.getRoom(0, 1);
+		boolean isItBat = r1.hasBats();
+		return isItBat;
+	}
+	
+	public boolean TestIfMapHasNormalRoom()
+	{
+		Room roomNormal =  new Room(0, 1, Room.RoomType.Normal);
+		GameContext.game.setRoom(0, 1, roomNormal);
+		Room r1 = GameContext.game.getRoom(0, 1);
+		boolean isItBat = r1.hasBats();
+		boolean isItPit = r1.hasPit();
+		boolean isItNormal = true;
+		if(isItBat == false && isItPit == false)
+			isItNormal = true;
+			else
+				isItNormal = false;
+		return isItNormal;
+	}
 
+	
+	public boolean TestMoveRickRandomly()
+	{
+		Game game = new Game(3, 3);
+		GameEntity rick = game.getRick();
+		BoardPosition position = rick.getCurrentPosition();
+		int movedNorth = 0;
+		int movedSouth = 0;
+		int movedEast = 0;
+		int movedWest = 0;
+		int rested = 0;
+		
+		for (int i = 0; i < 10000; i++) {
+			position.setXPos(1);
+			position.setYPos(1);
+			
+			game.moveRickRandom();
+			
+			if (position.getYPos() == 0) {
+				movedNorth++;
+			} else if (position.getYPos() == 2) {
+				movedSouth++;
+			} else  if (position.getXPos() == 0) {
+				movedWest++;
+			} else if (position.getXPos() == 2) {
+				movedEast++;
+			} else if (position.getXPos() == 1 && position.getYPos() == 1) {
+				rested++;
+			}
+		}
+	
+		boolean ricksMovesRandomly = false;
+		if(movedNorth + movedSouth + movedEast + movedWest + rested == 10000)
+		{
+			if(movedNorth > 1500 && movedSouth > 1500 &&movedEast > 1500 &&movedWest > 1500 && rested > 1500)
+			{
+				ricksMovesRandomly = true;
+			}
+			else ricksMovesRandomly = false;
+		}	
+		return ricksMovesRandomly;
+	}
+
+	
+	
+	public boolean doesPlayerDieInPitRoom()
+	{
+		int row = 0, col = 1;
+		boolean isPlayerDead = false;
+
+		Room pitRoom = new Room(row, col, Room.RoomType.PitRoom);		
+		GameContext.game.setRoom(row, col, pitRoom);
+		GameContext.game.movePlayer('E');
+		if(GameContext.game.getPlayer().isAlive()){
+			isPlayerDead = false;
+		}
+		else isPlayerDead = true;
+
+		return isPlayerDead;
+	}
+	
+	public boolean doesRickKillPlayer()
+	{
+		boolean dead =false;
+		Game theGame = new Game(5, 5);
+		
+		GameEntity theplayer = theGame.getPlayer();
+		GameEntity wumbus = theGame.getRick();
+		BoardPosition positionRick = wumbus.getCurrentPosition();
+		BoardPosition positionPlayer = theplayer.getCurrentPosition();
+		int x = positionRick.getXPos();
+		int y = positionRick.getYPos();
+		int x1 = positionPlayer.getXPos();
+		int y1 = positionPlayer.getYPos();
+		
+		theGame.movePlayer('E');
+		theGame.movePlayer('E');
+		GameContext.game.movePlayer('E');
+		GameContext.game.movePlayer('E');
+		GameContext.game.movePlayer('S');
+		GameContext.game.movePlayer('S');
+		GameContext.game.movePlayer('S');
+		GameContext.game.movePlayer('S');
+		
+		return theplayer.isAlive();
+
+		
+	}
 }
