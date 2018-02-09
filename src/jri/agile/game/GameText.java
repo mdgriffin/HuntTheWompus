ef10b84ed33cbc82a522eea272ae477c263c343f
Michael Griffin <mdgriffin064@gmail.com>
2018-02-09 10:28:43 +0000

    Added file containing static game text strings

---
+++ src/jri/agile/game/GameText.java
@@ -0,0 +1,44 @@
+package jri.agile.game;
+
+public class GameText {
+	public static final String welcome = "Welcome to Hunt the Wombus!";
+	public static final String killedRick = "You killed Rick!";
+	public static final String killedByRick = "You were killed by Rick!";
+	public static final String killedByArrow = "You took an arrow to the knee!";
+	public static final String killedByPit = "You fell into the Pit";
+	public static final String playerNoArrows = "Oh, no arrows left";
+	public static final String batRoom = "You hear the flapping of wings nearby.";
+	public static final String movedByBats = "Ah, bats have moved caught you and are dragging you to a different room!";
+	public static final String movedToNewRoom = "You moved to a new room";
+	public static final String movedNull = "Have not moved..";
+	public static final String pitRoom = "You feel a cool breeze nearby.";
+	public static final String rest = "Resting";
+	public static final String pickedUpArrows = "You picked up %d arrow(s)";
+	public static final String playAgain = "\nEnter (Y)es to play another game: ";
+	public static final String goodbye = "\n\nGoodbye, thanks for playing";
+	public static final String pointer = "\n> ";
+	public static final String invalidCommand = "\nInvalid command please enter again";
+	public static final String rickText1 = "You hear something near by, \"....give.....up\"";
+	public static final String rickText2 = "You hear something near by, \".....let...down\"";
+	public static final String rickText3 = "You hear something near by, \"....run........\"";
+	public static final String rickText4 = "You hear something near by, \".......Hurt You\"";
+	public static final String rickText5 = "You hear something near by, \"....say goodbye\"";
+	
+	public static final String instructions = "\nTo Move enter (M)ove followed by the direction (N, S, E, W)" +
+			"\nTo Shoot enter (S)hoot followed by the direction (N, S, E, W)" +
+			"\nTo see these options again, type (H)elp" +
+			"\nTo quit the game enter (Q)uit";
+
+	public static final char commandHelp = 'H';
+	public static final char commandPrintMap = 'P';
+	public static final char commandMove = 'M';
+	public static final char commandRest = 'R';
+	public static final char commandQuit = 'Q';
+	public static final char commandShoot = 'S';
+	public static final char commandNorth = 'N';
+	public static final char commandSouth = 'S';
+	public static final char commandEast = 'E';
+	public static final char commandWest = 'W';
+	public static final char commandYes = 'Y';
+
+}
